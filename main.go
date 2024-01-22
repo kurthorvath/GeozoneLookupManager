dc9d18cf35e5f83f331810b8dac91b49c855e916
Kurt Horvath <kurtklaus.horvath@gmail.com>
2024-01-22 11:24:26 +0100

    add file handling for geojson

--- main.go
+++ main.go
@@ -4,9 +4,52 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type GeoJson struct {
+	Type string `json:"type"`
+	Name string `json:"name"`
+	Crs  struct {
+		Type       string `json:"type"`
+		Properties struct {
+			Name string `json:"name"`
+		} `json:"properties"`
+	} `json:"crs"`
+	Features []struct {
+		Type       string `json:"type"`
+		Properties struct {
+			Name string `json:"name"`
+			Iso  string `json:"iso"`
+		} `json:"properties"`
+		Geometry struct {
+			Type        string          `json:"type"`
+			Coordinates [][][][]float64 `json:"coordinates"`
+		} `json:"geometry"`
+	} `json:"features"`
+}
+
+type GeoJsonData struct {
+	DB []GeoJson
+}
+
+type GeoJsonDB struct {
+	DB []GeoJsonData
+}
+
+func (d GeoJsonDB) read() {
+	files, err := ioutil.ReadDir("files/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name(), file.IsDir())
+	}
+
+}
 
 func forward2Request(LD string) {
 	posturl := "http://127.0.0.1:7900"
@@ -16,6 +59,9 @@ func forward2Request(LD string) {
 	if err != nil {
 		panic(err)
 	}
+
+	var myDB GeoJsonDB
+	myDB.read()
 
 	client := &http.Client{}
 	res, err := client.Do(r)
